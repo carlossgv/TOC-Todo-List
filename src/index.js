8dd815e4814c1d535afa0307c1dee916d6f2c3eb
Carlos Gonzalez <carlossgv@gmail.com>
2021-02-16 22:13:22 -0300

    local name and some css implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
-$(document).ready(()=>{
+$(document).ready(() => {
+  setName();
 
-    
-    console.log('Document Loaded!')
-})
+  const name = localStorage.getItem('name');
+
+  $('.user-info').html(`${name}'s Projects`);
+
+  console.log('Document Loaded!!');
+});
+
+let setName = () => {
+  if (localStorage.getItem('name') === null) {
+    const name = prompt("What's your name?");
+    if (name === '' || name === null) {
+      setName();
+    } else {
+      localStorage.setItem('name', name);
+    }
+  }
+};
