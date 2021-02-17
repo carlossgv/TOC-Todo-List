97e1e7914bbd20c6430d156e7be0a8dca5a7af9f
Carlos Gonzalez <carlossgv@gmail.com>
2021-02-17 16:47:56 -0300

    add and load projects implemented

--- src/index.js
+++ src/index.js
@@ -4,21 +4,90 @@ $(document).ready(() => {
   setName();
 
   $('form[name="add-project"]').submit(function (e) {
+    console.log(e.target[0].value);
     addNewProject(e.target[0].value);
+    e.target[0].value = '';
     e.preventDefault();
   });
 
-  console.log('Document Loaded!!');
+  // console.log(JSON.parse(window.localStorage.getItem('projectsList')));
+
+  loadProjects();
+
+  console.log('Document Loaded!!!');
 });
+
+// loadProjects if false wont add doms again, just return lists
+const loadProjects = (run = true) => {
+  if (JSON.parse(window.localStorage.getItem('projectsList'))) {
+    let projectsList = JSON.parse(window.localStorage.getItem('projectsList'));
+    let projectsListNames = JSON.parse(
+      window.localStorage.getItem('projectsListNames')
+    );
+
+    if (run) {
+      projectsList.forEach((element) => {
+        addProjectDOM(element.name);
+      });
+    }
+
+    return { projectsList, projectsListNames };
+  } else {
+    let projectsList = [];
+    let projectsListNames = [];
+
+    return { projectsList, projectsListNames };
+  }
+};
 
 class Project {
   constructor(name) {
     this.name = name;
+    this.tasks = [];
   }
 
+  addTask(task) {
+    this.tasks.push(task);
+  }
 }
 
-let addNewProject = (name) => {
+class Task {
+  constructor(project, title, description, dueDate, priority) {
+    this.project = project;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+const addNewProject = (name) => {
+  let lists = loadProjects(false);
+
+  if (name == '' || name == null) {
+    console.log('Proyect needs a name');
+  } else if (lists.projectsListNames.includes(name)) {
+    console.log('Proyect already exists');
+  } else {
+    lists.projectsListNames.push(name);
+
+    let newProyect = new Project(name);
+    lists.projectsList.push(newProyect);
+
+    addProjectDOM(name);
+
+    window.localStorage.setItem(
+      'projectsList',
+      JSON.stringify(lists.projectsList)
+    );
+    window.localStorage.setItem(
+      'projectsListNames',
+      JSON.stringify(lists.projectsListNames)
+    );
+  }
+};
+
+const addProjectDOM = (name) => {
   const projects = $('.projects');
 
   const div = document.createElement('div');
@@ -34,7 +103,7 @@ let addNewProject = (name) => {
   projects.append(div);
 };
 
-let setName = () => {
+const setName = () => {
   if (localStorage.getItem('name') === null) {
     const name = prompt("What's your name?");
     if (name === '' || name === null) {
